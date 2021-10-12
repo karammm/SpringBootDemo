95e9e320ec2c38500b9b3fafb21b95a2dc26c911
Karamijit <karamjitverma9@gmail.com>
2021-10-12 23:18:17 +0530

    First commit

---
+++ src/main/java/com/example/demo/jarvis.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+//hey spring framework this is a class for which i need an object
+//Spring framework works on singleton design pattern
+//So in singleton you get only one instance
+@Component
+@Scope(value="prototype")//by creating prototype it means it will not create an instance by default
+//the only moment you ask for getBean at that point it will create an object for you
+public class jarvis {
+	private int jid;
+	public jarvis() {
+		super();
+		System.out.println("***********Object Created**********");
+	}
+	private String jname;
+	private String tech;
+	public int getJid() {
+		return jid;
+	}
+	public void setJid(int jid) {
+		this.jid = jid;
+	}
+	public String getJname() {
+		return jname;
+	}
+	public void setJname(String jname) {
+		this.jname = jname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	public void show() {
+		System.out.println("Hey component");
+	}
+}
